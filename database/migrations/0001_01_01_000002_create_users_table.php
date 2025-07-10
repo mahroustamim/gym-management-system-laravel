9fdd00a9c250f70ec3570d5368a8e7abbf8035be
mahroustamim <mahroustamim@gmail.com>
2025-07-10 19:35:36 +0300

    feat create migrations

--- database/migrations/0001_01_01_000002_create_users_table.php
+++ database/migrations/0001_01_01_000002_create_users_table.php
@@ -13,10 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('gym_id')->nullable()->constrained('gyms')->nullOnDelete();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('social_id')->nullable();
+            $table->string('social_type')->nullable();
+            $table->decimal('money_balance', 10, 2)->default(0);
+            $table->enum('role', ['saas_admin', 'gym_admin', 'employee']);
+            $table->json('permissions')->nullable();
+            $table->string('otp')->nullable();
+            $table->timestamp('otp_expires_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_07_09_113715_create_trainers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trainers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('gym_id')->constrained();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('specialty')->nullable();
+            $table->enum('status', ['active', 'inactive']);
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainers');
+    }
+};
